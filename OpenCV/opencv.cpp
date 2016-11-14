ba51df35b6f81031ff3f95b154c26875abafe9a7
Jianyu.Wang <873869027@qq.com>
2016-11-14 21:56:22 +0800

    Reset resources

---
+++ OpenCV/opencv.cpp
@@ -0,0 +1,22 @@
+#include <opencv2\opencv.hpp>  
+#include <iostream>  
+#include <string>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	Mat img = imread("opencv.jpg");
+
+	if (img.empty())
+	{
+		cout << "cannot found the picture in current directory.";
+		return 0;
+	}
+
+	imshow("imageshow", img);
+	waitKey();
+
+	return 0;
+}
