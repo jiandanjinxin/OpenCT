23af11014af0488d453eef04e29a00bb5f883493
Jianyu.Wang <873869027@qq.com>
2016-12-22 20:37:23 +0800

    Candidate class handle world data of position

---
+++ OpenCV/HCandidate.h
@@ -0,0 +1,20 @@
+#ifndef __CANDIDATE_DATA_H  
+#define __CANDIDATE_DATA_H
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class Candidate
+{
+public:
+
+
+
+private:
+
+};
+
+
+#endif
