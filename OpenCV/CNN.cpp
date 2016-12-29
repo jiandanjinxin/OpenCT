9e5f7e7cdcfee4565a2466507018456521dfab0f
Jianyu.Wang <873869027@qq.com>
2016-12-29 12:53:17 +0800

    CNN init function

--- OpenCV/CNN.cpp
+++ OpenCV/CNN.cpp
@@ -86,6 +86,44 @@ void CNN::init_variable(double *val, double c, int len)
 		val[i] = c;
 }
 
+void CNN::init()
+{
+	int len_input_train = WIDTH_IMAGE_INPUT_CNN * HEIGHT_IMAGE_INPUT_CNN * NUM_PATTERNS_TRAIN_CNN;
+	data_input_train = new double[len_input_train];
+	init_variable(data_input_train, -1.0, len_input_train);
 
+	int len_output_train = NUM_MAP_OUTPUT_CNN * NUM_PATTERNS_TRAIN_CNN;
+	data_output_train = new double[len_output_train];
+	init_variable(data_output_train, -0.8, len_output_train);
+
+	int len_input_test = WIDTH_IMAGE_INPUT_CNN * HEIGHT_IMAGE_INPUT_CNN * NUM_PATTERNS_TEST_CNN;
+	data_input_test = new double[len_input_test];
+	init_variable(data_input_test, -1.0, len_input_test);
+
+	int len_output_test = NUM_MAP_OUTPUT_CNN * NUM_PATTERNS_TEST_CNN;
+	data_output_test = new double[len_output_test];
+	init_variable(data_output_test, -0.8, len_output_test);
+
+	std::fill(E_weight_C1, E_weight_C1 + LEN_WEIGHT_C1_CNN, 0.0);
+	std::fill(E_bias_C1, E_bias_C1 + LEN_BIAS_C1_CNN, 0.0);
+	std::fill(E_weight_S2, E_weight_S2 + LEN_WEIGHT_S2_CNN, 0.0);
+	std::fill(E_bias_S2, E_bias_S2 + LEN_BIAS_S2_CNN, 0.0);
+	std::fill(E_weight_C3, E_weight_C3 + LEN_WEIGHT_C3_CNN, 0.0);
+	std::fill(E_bias_C3, E_bias_C3 + LEN_BIAS_C3_CNN, 0.0);
+	std::fill(E_weight_S4, E_weight_S4 + LEN_WEIGHT_S4_CNN, 0.0);
+	std::fill(E_bias_S4, E_bias_S4 + LEN_BIAS_S4_CNN, 0.0);
+    
+	E_weight_C5 = new double[LEN_WEIGHT_C5_CNN];
+	std::fill(E_weight_C5, E_weight_C5 + LEN_WEIGHT_C5_CNN, 0.0);
+	E_bias_C5 = new double[LEN_BIAS_C5_CNN];
+	std::fill(E_bias_C5, E_bias_C5 + LEN_BIAS_C5_CNN, 0.0);
+	E_weight_output = new double[LEN_WEIGHT_OUTPUT_CNN];
+	std::fill(E_weight_output, E_weight_output + LEN_WEIGHT_OUTPUT_CNN, 0.0);
+	E_bias_output = new double[LEN_BIAS_OUTPUT_CNN];
+	std::fill(E_bias_output, E_bias_output + LEN_BIAS_OUTPUT_CNN, 0.0);
+
+	initWeightThreshold();
+	getSrcData();
+}
 
 }
