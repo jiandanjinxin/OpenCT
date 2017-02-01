366fb18a2e7d5532cf68601b399047272ecea230
Jianyu.Wang <873869027@qq.com>
2017-02-01 16:18:20 +0800

    serializeString

--- EasyCNN/ConvolutionLayer.cpp
+++ EasyCNN/ConvolutionLayer.cpp
@@ -27,3 +27,60 @@ void EasyCNN::ConvolutionLayer::setParamaters(const ParamSize _kernelSize, const
 	heightStep = _heightStep;
 	enabledBias = _enabledBias;
 }
+
+std::string EasyCNN::ConvolutionLayer::serializeToString() const
+{
+	const std::string spliter = " ";
+	std::stringstream ss;
+	//layer desc
+	ss << getLayerType() << spliter
+		<< kernelSize.number << spliter << kernelSize.channels << spliter << kernelSize.width << spliter << kernelSize.height << spliter
+		<< widthStep << spliter << heightStep << spliter << enabledBias << spliter;
+	//weight
+	const auto kernel = kernelData->getData().get();
+	for (size_t i = 0; i < kernelSize._4DSize(); i++)
+	{
+		ss << kernel[i] << spliter;
+	}
+	//bias
+	if (enabledBias)
+	{
+		const auto bias = biasData->getData().get();
+		const auto biasSize = biasData->getSize();
+		for (size_t i = 0; i < biasSize._4DSize(); i++)
+		{
+			ss << bias[i] << spliter;
+		}
+	}
+	return ss.str();
+}
+
+void EasyCNN::ConvolutionLayer::serializeFromString(const std::string content)
+{
+	std::stringstream ss(content);
+	//layer desc
+	std::string _layerType;
+	ss >> _layerType
+		>> kernelSize.number >> kernelSize.channels >> kernelSize.width >> kernelSize.height
+		>> widthStep >> heightStep >> enabledBias;
+	easyAssert(_layerType == layerType, "layer type is invalidate.");
+	solveInnerParams();
+	//weight
+	auto kernel = kernelData->getData().get();
+	for (size_t i = 0; i < kernelSize._4DSize(); i++)
+	{
+		ss >> kernel[i];
+	}
+	//bias
+	if (enabledBias)
+	{
+		const auto bias = biasData->getData().get();
+		const auto biasSize = biasData->getSize();
+		for (size_t i = 0; i < biasSize._4DSize(); i++)
+		{
+			ss >> bias[i];
+		}
+	}
+}
+
+DEFINE_LAYER_TYPE(EasyCNN::ConvolutionLayer, "ConvolutionLayer");
