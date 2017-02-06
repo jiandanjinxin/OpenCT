01e564ab1855077e9028a51dfa12f4cfd9b0f2c4
Jianyu.Wang <873869027@qq.com>
2017-02-06 18:54:03 +0800

    Sigmoid forward

--- EasyCNN/ActivationLayer.cpp
+++ EasyCNN/ActivationLayer.cpp
@@ -32,3 +32,27 @@ static inline float sigmodDfOperator(const float x)
 	return x * (1.0f - x);
 }
 
+//Sigmoid forward
+void EasyCNN::SigmodLayer::forward(const std::shared_ptr<DataBucket> prevDataBucket, std::shared_ptr<DataBucket> nextDataBucket)
+{
+	const DataSize prevDataSize = prevDataBucket->getSize();
+	const DataSize nextDataSize = nextDataBucket->getSize();
+
+	const float* prevRawData = prevDataBucket->getData().get();
+	float* nextRawData = nextDataBucket->getData().get();
+
+	for (size_t nn = 0; nn < nextDataSize.number; nn++)
+	{
+		for (size_t nc = 0; nc < nextDataSize.channels; nc++)
+		{
+			for (size_t nh = 0; nh < nextDataSize.height; nh++)
+			{
+				for (size_t nw = 0; nw < nextDataSize.width; nw++)
+				{
+					const size_t nextDataIdx = nextDataSize.getIndex(nn, nc, nh, nw);
+					nextRawData[nextDataIdx] = sigmodOperator(prevRawData[nextDataIdx]);
+				}
+			}
+		}
+	}
+}
