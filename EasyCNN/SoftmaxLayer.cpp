d9019965fd95ae9e8e9cde742b0bdc1e00bb9fcb
Jianyu.Wang <873869027@qq.com>
2017-02-15 20:18:50 +0800

    SoftmaxLayer forward.

--- EasyCNN/SoftmaxLayer.cpp
+++ EasyCNN/SoftmaxLayer.cpp
@@ -10,3 +10,43 @@ EasyCNN::SoftmaxLayer::~SoftmaxLayer()
 {
 
 }
+
+DEFINE_LAYER_TYPE(EasyCNN::SoftmaxLayer, "SoftmaxLayer");
+std::string EasyCNN::SoftmaxLayer::getLayerType() const
+{
+	return layerType;
+}
+
+//SoftmaxLayer forward
+void EasyCNN::SoftmaxLayer::forward(const std::shared_ptr<DataBucket> prevDataBucket, std::shared_ptr<DataBucket> nextDataBucket)
+{
+	const DataSize prevDataSize = prevDataBucket->getSize();
+	const DataSize nextDataSize = nextDataBucket->getSize();
+
+	for (size_t nn = 0; nn < nextDataSize.number; nn++)
+	{
+		const float* prevData = prevDataBucket->getData().get() + nn * prevDataSize._3DSize();
+		float* nextData = nextDataBucket->getData().get() + nn * nextDataSize._3DSize();
+
+		//step1 : find max value
+		float maxVal = prevData[0];
+		for (size_t prevDataIdx = 0; prevDataIdx < prevDataSize._3DSize(); prevDataIdx++)
+		{
+			maxVal = std::max(maxVal, prevData[prevDataIdx]);
+		}
+
+		//step2 : sum
+		float sum = 0;
+		for (size_t prevDataIdx = 0; prevDataIdx < prevDataSize._3DSize(); prevDataIdx++)
+		{
+			nextData[prevDataIdx] = std::exp(prevData[prevDataIdx] - maxVal);
+			sum += nextData[prevDataIdx];
+		}
+
+		//step3 : div
+		for (size_t prevDataIdx = 0; prevDataIdx < prevDataSize._3DSize(); prevDataIdx++)
+		{
+			nextData[prevDataIdx] = nextData[prevDataIdx] / sum;
+		}
+	}
+}
