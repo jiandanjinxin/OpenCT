68628c9822c98e4cf146cba2a38a9e174a34aac2
Jianyu.Wang <873869027@qq.com>
2017-02-16 16:07:29 +0800

    SoftmaxLayer backward.

--- EasyCNN/SoftmaxLayer.cpp
+++ EasyCNN/SoftmaxLayer.cpp
@@ -50,3 +50,49 @@ void EasyCNN::SoftmaxLayer::forward(const std::shared_ptr<DataBucket> prevDataBu
 		}
 	}
 }
+
+//SoftmaxLayer backward
+void EasyCNN::SoftmaxLayer::backward(std::shared_ptr<DataBucket> prevDataBucket, const std::shared_ptr<DataBucket> nextDataBucket, std::shared_ptr<DataBucket>& nextDiffBucket)
+{
+	easyAssert(getPhase() == Phase::Train, "backward only in train phase.")
+	const DataSize prevDataSize = prevDataBucket->getSize();
+	const DataSize nextDataSize = nextDataBucket->getSize();
+	const DataSize nextDiffSize = nextDiffBucket->getSize();
+	easyAssert(prevDataSize == nextDataSize, "data size must be equal!");
+	easyAssert(nextDiffSize == nextDataSize, "next data's and diff's size must be equal! ");
+
+	//update prevDiff data
+	const DataSize prevDiffSize(prevDataSize.number, prevDataSize.channels, prevDataSize.height, prevDataSize.width);
+	easyAssert(prevDiffSize == nextDiffSize, "diff size must be equal!");
+	std::shared_ptr<DataBucket> prevDiffBucket(std::make_shared<DataBucket>(prevDiffSize));
+	prevDiffBucket->fillData(0.0f);
+
+	for (size_t pn = 0; pn < prevDataSize.number; pn++)
+	{
+		const float* prevData = prevDataBucket->getData().get() + pn*prevDataSize._3DSize();
+		const float* nextData = nextDataBucket->getData().get() + pn*nextDataSize._3DSize();
+		const float* nextDiff = nextDiffBucket->getData().get() + pn*nextDiffSize._3DSize();
+
+		float* prevDiff = prevDiffBucket->getData().get() + pn * prevDiffSize._3DSize();
+
+		for (size_t prevDiffIdx = 0; prevDiffIdx < prevDiffSize._3DSize(); prevDiffIdx++)
+		{
+			for (size_t nextDiffIdx = 0; nextDiffIdx < nextDiffSize._3DSize(); nextDiffIdx++)
+			{
+				if (nextDiffIdx == prevDiffIdx)
+				{
+					prevDiff[prevDiffIdx] += nextData[prevDiffIdx] * (1.0f - nextData[prevDiffIdx]) * nextDiff[nextDiffIdx];
+				}
+				else
+				{
+					prevDiff[prevDiffIdx] -= nextData[prevDiffIdx] * nextData[nextDiffIdx] * nextDiff[nextDiffIdx];
+				}
+			}
+		}
+	}
+
+	//update this layer's param
+	//softmax layer : nop
+
+	nextDiffBucket = prevDiffBucket;
+}
