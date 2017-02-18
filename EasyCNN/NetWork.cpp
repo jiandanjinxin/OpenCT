41bc9f0ff0c859d61bd5feb6536ebd96f29e38d8
Jianyu.Wang <873869027@qq.com>
2017-02-18 22:24:15 +0800

    NetWork forward finished.

--- EasyCNN/NetWork.cpp
+++ EasyCNN/NetWork.cpp
@@ -32,6 +32,7 @@ void EasyCNN::NetWork::setPhase(Phase phase)
 	logVerbose("NetWork setPhase end.");
 }
 
+// common
 EasyCNN::Phase EasyCNN::NetWork::getPhase() const
 {
 	return phase;
@@ -90,4 +91,64 @@ std::shared_ptr<EasyCNN::Layer> EasyCNN::NetWork::createLayerByType(const std::s
 		easyAssert(false, "can't goto here.");
 		return nullptr;
 	}
+}
+
+std::vector<std::shared_ptr<EasyCNN::Layer>> EasyCNN::NetWork::serializeFromString(const std::string content)
+{
+	int number = 1;
+	int channels = 0;
+	int width = 0;
+	int height = 0;
+	std::stringstream ss(content);
+	ss >> channels >> width >> height;
+	setInputSize(DataSize(number, channels, width, height));
+	std::vector<std::shared_ptr<EasyCNN::Layer>> tmpLayers;
+	while (!ss.eof())
+	{
+		std::string layerType;
+		ss >> layerType;
+		if (layerType.empty())
+		{
+			continue;
+		}
+		std::shared_ptr<EasyCNN::Layer> layer = createLayerByType(layerType);
+		easyAssert(layer.get() != nullptr, "layer can't be null.");
+		tmpLayers.push_back(layer);
+	}
+	return tmpLayers;
+}
+
+std::shared_ptr<EasyCNN::DataBucket> EasyCNN::NetWork::forward(const std::shared_ptr<DataBucket> inputDataBucket)
+{
+	logVerbose("NetWork forward begin.");
+	easyAssert(layers.size() > 1, "layer count is less than 2.");
+	easyAssert(layers[0]->getLayerType() == InputLayer::layerType, "first layer is not input layer.");
+	easyAssert(dataBuckets.size() > 0, "data buckets is not ready.");
+
+	//copy data from inputDataBucket
+	//reshape data bucket
+	const auto oldNumber = dataBuckets[0]->getSize().number;
+	const auto newNumber = inputDataBucket->getSize().number;
+
+	if (newNumber != oldNumber)
+	{
+		for (size_t i = 0; i < dataBuckets.size(); i++)
+		{
+			auto newSize = dataBuckets[i]->getSize();
+			newSize.number = newNumber;
+			dataBuckets[i].reset(new DataBucket(newSize));
+		}
+	}
+
+	inputDataBucket->cloneTo(*dataBuckets[0]);
+
+	for (size_t i = 0; i < layers.size(); i++)
+	{
+		logVerbose("NetWork layer[%d](%s) forward begin.", i, layers[i]->getLayerType().c_str());
+		layers[i]->forward(dataBuckets[i], dataBuckets[i + 1]);
+		logVerbose("NetWork layer[%d](%s) forward end.", i, layers[i]->getLayerType().c_str());
+	}
+
+	logVerbose("NetWork forward end.");
+	return dataBuckets[dataBuckets.size() - 1];
 }
